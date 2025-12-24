0178047ecd8007c2c674eda56e2605be69980171
Victory Wisdom Okon <etangdgm001@gmail.com>
2025-12-24 09:16:50 +0100

    updates -- victory

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-// import router from './router'
 import { createBootstrap } from 'bootstrap-vue-next'
 
 // Import Bootstrap CSS
@@ -18,7 +17,6 @@ import "@fortawesome/fontawesome-free/js/all.js";
 const app = createApp(App)
 
 app.use(createPinia())
-// app.use(router)
 app.use(createBootstrap())
 
 app.mount('#app')
